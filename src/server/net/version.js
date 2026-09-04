c2868e0fe355268a0413bee4c8366468a637ae74
Jason Glenn Evans <gvbvdxxalt2@outlook.com>
2026-09-04 08:26:00 -0400

    Compatibility update

--- src/server/net/version.js
+++ src/server/net/version.js
@@ -1,5 +1,9 @@
 class SRB2WebRelayProtocol {
-    static RELAY_PROTOCOL = "SRB2W_QRTC_V1";
+    static RELAY_PROTOCOL = "SRB2W_QRTC_V2";
+
+    static GAME_NAME = "SRB2 Web";
+
+    static GAME_ID = "SRB2";
 }
 
 module.exports = SRB2WebRelayProtocol;
